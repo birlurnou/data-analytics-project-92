663fa7fa7810566ed53c1508fb9d3c6b1e4408ae
birlurnou <111186103+birlurnou@users.noreply.github.com>
2025-08-25 11:43:22 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -5,6 +5,8 @@ select distinct count(customer_id) as "customers_count"
 from customers;
 
 
+-- Анализ отдела продаж
+
 -- 1 отчёт
 -- Здесь в одном запросе я формирую имя, считаю кол-во операций и общую выручку.
 
@@ -97,3 +99,70 @@ select
     day_of_week,
     income
 from tab2;
+
+-- Анализ покупателей
+
+-- 1 отчёт
+-- Объединение трёх подзапросов, каждый из которых считает количество покупателей в определённом возрастном диапазоне
+
+(select 
+    '16-25' as age_category,
+    count(*) as age_count
+from customers
+where age between 16 and 25)
+union
+(select 
+    '26-40' as age_category,
+    count(*) as age_count
+from customers
+where age between 26 and 40)
+union
+(select 
+    '40+' as age_category,
+    count(*) as age_count
+from customers
+where age > 40)
+;
+
+-- 2 отчёт
+-- Я делаю выборку количества уникальных покупателей и суммы их выручки с группировкой под месяцам
+
+select 
+    to_char(sale_date, 'YYYY-MM') as selling_month,
+    count(distinct s.customer_id) as total_customers, 
+    floor(sum(s.quantity*p.price)) as income
+from sales s
+inner join products p on s.product_id = p.product_id
+group by to_char(sale_date, 'YYYY-MM')
+order by selling_month;
+
+-- 3 отчёт
+
+/*
+
+Я создаю табличное выражение с данными покупателя, продавца и даты сделки, когда цена была равна 0,
+также добавляю оконную функцию row_number с сортировкой по дате, 
+чтобы в основном запросе были данные не всех покупок, а только одной первой
+
+*/
+
+with tab as (
+    select 
+        c.first_name || ' ' || c.last_name as customer,
+        s.sale_date::date as sale_date,
+        concat(e.first_name, ' ', e.last_name) as seller,
+        row_number() over (partition by c.customer_id order by s.sale_date) as rn
+    from sales s
+    inner join employees e on e.employee_id = s.sales_person_id
+    inner join customers c on c.customer_id = s.customer_id
+    inner join products p on p.product_id = s.product_id
+    where s.quantity * p.price = 0
+    order by c.customer_id
+)
+
+select 
+    customer,
+    sale_date,
+    seller
+from tab
+where rn = 1;
