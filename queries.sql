5dd09d3c8c0ca3e386a693068df0bdfcbaafd16d
birlurnou <111186103+birlurnou@users.noreply.github.com>
2025-08-27 13:15:58 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 -- Общее количество покупателей.
 -- Запрос делает выборку количества уникальных id покупателей из таблицы customers.
 
-select distinct count(customer_id) as "customers_count"
+select distinct count(customer_id) as customers_count
 from customers;
 
 
 -- Анализ отдела продаж
 
--- 1 отчёт
+-- 1 отчёт (top_10_total_income)
 -- Здесь в одном запросе я формирую имя, считаю кол-во операций и общую выручку.
 
 select 
@@ -18,10 +18,10 @@ from sales s
 inner join products p on s.product_id = p.product_id
 inner join employees e on s.sales_person_id = e.employee_id
 group by s.sales_person_id, concat(e.first_name, ' ', e.last_name)
-order by income desc;
+order by income desc limit 10;
 
 
--- 2 отчёт
+-- 2 отчёт (lowest_average_income)
 
 /*
 
@@ -55,7 +55,7 @@ where ai.average_income < tai.total_avg
 order by ai.average_income;
 
 
--- 3 отчёт
+-- 3 отчёт (day_of_the_week_income)
 
 /*
 
@@ -102,8 +102,8 @@ from tab2;
 
 -- Анализ покупателей
 
--- 1 отчёт
--- Объединение трёх подзапросов, каждый из которых считает количество покупателей в определённом возрастном диапазоне
+-- 1 отчёт (age_groups)
+-- Объединение трёх подзапросов, каждый из которых считает количество покупателей в определённом возрастном диапазоне.
 
 (select 
     '16-25' as age_category,
@@ -124,8 +124,8 @@ from customers
 where age > 40)
 ;
 
--- 2 отчёт
--- Я делаю выборку количества уникальных покупателей и суммы их выручки с группировкой под месяцам
+-- 2 отчёт (customers_by_month)
+-- Я делаю выборку количества уникальных покупателей и суммы их выручки с группировкой под месяцам.
 
 select 
     to_char(sale_date, 'YYYY-MM') as selling_month,
@@ -136,13 +136,13 @@ inner join products p on s.product_id = p.product_id
 group by to_char(sale_date, 'YYYY-MM')
 order by selling_month;
 
--- 3 отчёт
+-- 3 отчёт (special_offer)
 
 /*
 
 Я создаю табличное выражение с данными покупателя, продавца и даты сделки, когда цена была равна 0,
 также добавляю оконную функцию row_number с сортировкой по дате, 
-чтобы в основном запросе были данные не всех покупок, а только одной первой
+чтобы в основном запросе были данные не всех покупок, а только одной первой.
 
 */
 
@@ -159,10 +159,144 @@ with tab as (
     where s.quantity * p.price = 0
     order by c.customer_id
 )
-
 select 
     customer,
     sale_date,
     seller
 from tab
 where rn = 1;
+
+/* 
+
+Мне не понравилось делать дашборд и презентацию с имеющимися данными,
+поэтому я сделал ещё несколько выборок:
+
+-- топ-10 самых продаваемых продуктов
+
+select 
+    products.name as product_name,
+    sum(sales.quantity) as quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join products on products.product_id = sales.product_id
+group by products.name
+order by total_amount desc limit 10;
+
+-- топ-10 наименее продаваемых продуктов
+
+select 
+    products.name as product_name,
+    sum(sales.quantity) as quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join products on products.product_id = sales.product_id
+where products.price <> 0
+group by products.name
+order by total_amount asc limit 10;
+
+-- доля выручки с каждого продукта
+
+select 
+    products.name as product_name,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join products on products.product_id = sales.product_id
+group by products.name
+order by total_amount desc;
+
+-- топ-10 клиентов
+
+select 
+    customers.first_name || ' ' || customers.last_name as customer,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+group by customers.first_name || ' ' || customers.last_name
+order by total_amount desc limit 10;
+
+-- вклад клиентов по возрастной группе
+
+(select 
+    '16-25' as age_category,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age between 16 and 25)
+union
+(select 
+    '26-40' as age_category,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age between 26 and 40)
+union
+(select 
+    '40+' as age_category,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age > 40)
+;
+
+-- топ-10 товаров каждой возрастной группы
+
+(select 
+    '16-25' as age_category,
+    products.name as product_name,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age between 16 and 25
+group by products.name
+order by total_amount desc, total_quantity desc limit 10)
+union
+(select 
+    '26-40' as age_category,
+    products.name as product_name,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age between 26 and 40
+group by products.name
+order by total_amount desc, total_quantity desc limit 10)
+union
+(select 
+    '40+' as age_category,
+    products.name as product_name,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on products.product_id = sales.product_id
+where customers.age > 40
+group by products.name
+order by total_amount desc, total_quantity desc limit 10)
+order by age_category asc, total_amount desc, total_quantity desc
+;
+
+-- все сотрудники (для исследования взаимосвязи)
+
+select 
+    employees.first_name || ' ' || employees.last_name as seller,
+    sum(sales.sales_id) as sale_count,
+    sum(sales.quantity) as total_quantity,
+    floor(sum(sales.quantity*products.price)) as total_amount
+from sales
+inner join products on products.product_id = sales.product_id
+inner join employees on employees.employee_id = sales.sales_person_id
+group by employees.first_name || ' ' || employees.last_name
+order by total_amount desc;
+
+*/
